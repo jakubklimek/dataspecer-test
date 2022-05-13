5aa8ceb33d27739db7b0596cc23e78b6238a9744
Jakub Klímek <jakub@jakubklimek.com>
2022-05-13 08:36:17 +0200

    Update script with rdfdiff

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ cp input.ttl data/rdf
 cp input.csv data/csv
 #pygmentize -g data/rdf/input.ttl
 sparql --data=data/rdf/input.ttl --query=$SPEC/$SCHEMA/query.sparql --results=turtle > data/rdf/sparqled.ttl
+rdfdiff data/rdf/input.ttl data/rdf/sparqled.ttl TTL TTL > data/rdf/sparqled-diff.txt
 #pygmentize -g data/rdf/sparqled.ttl
 riot --output=rdfxml data/rdf/input.ttl > data/rdf/input.rdf
 #pygmentize -g data/rdf/input.rdf
@@ -23,5 +24,6 @@ xmllint --schema $SPEC/$SCHEMA/schema.xsd data/xml/lowered.xml --noout > data/xm
 xslt3 -xsl:$SPEC/$SCHEMA/lifting.xslt -s:data/xml/lowered.xml -o:data/rdf/lifted.rdf -t
 #pygmentize -g data/rdf/lifted.rdf
 riot --formatted=turtle data/rdf/lifted.rdf > data/rdf/lifted.ttl 2> data/rdf/lifted-validation.txt
+rdfdiff data/rdf/input.ttl data/rdf/lifted.ttl TTL TTL > data/rdf/lifted-diff.txt
 rdf validate --format tabular --metadata $SPEC/$SCHEMA/schema.csv-metadata.json data/csv/input.csv > data/csv/validation.txt 2>&1
 rdf serialize --format tabular --minimal --decode-uri --output-format turtle --metadata $SPEC/$SCHEMA/schema.csv-metadata.json -o data/rdf/csv.ttl data/csv/input.csv
